165f0fd9eb41c64f9ca8a45f6faeca74b2b4f502
linuxguy467 <me@matthem.codes>
2024-06-02 17:30:04 -0400

    add simple file server

--- server.js
+++ server.js
@@ -1,13 +1,39 @@
 import http from 'node:http';
+import fs from 'node:fs/promises';
+import url from 'node:url';
+import path from 'node:path';
 
 const PORT = process.env.PORT;
 
-const server = http.createServer((req, res) => {
-  // res.setHeader('Content-Type', 'text/html');
-  // res.statusCode = 404;
+// Get current path
+const __filename = url.fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
-  res.writeHead(200, { 'Content-Type': 'text/html' });
-  res.end('<h1>Hello World</h1>');
+const server = http.createServer(async (req, res) => {
+  try {
+    // Check if GET request
+    if (req.method === 'GET') {
+      let filePath;
+      if (req.url === '/') {
+        filePath = path.join(__dirname, 'public', 'index.html');
+      } else if (req.url === '/about') {
+        filePath = path.join(__dirname, 'public', 'about.html');
+      } else {
+        throw new Error('Not Found');
+      }
+
+      const data = await fs.readFile(filePath);
+      res.setHeader('Content-Type', 'text/html');
+      res.write(data);
+      res.end();
+    } else {
+      throw new Error('Method not allowed.');
+    }
+  } catch (err) {
+    console.error(err);
+    res.writeHead(500, { 'Content-Type': 'text/plain' });
+    res.end('Server Error');
+  }
 });
 
 server.listen(PORT, () => {
